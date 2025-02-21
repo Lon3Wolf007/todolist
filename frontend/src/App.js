4446d95cc9851f738b4181e6b756f0ded0ec5930
Lon3Wolf007 <ketogju@gmail.com>
2025-02-21 19:21:20 +0530

    Added frontend folder with dependencies and code

---
+++ frontend/src/App.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/todos")
+      .then((response) => setTodos(response.data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const addTodo = () => {
+    if (!task.trim()) return;
+    axios.post("http://localhost:5000/api/todos", { task })
+      .then((response) => setTodos([...todos, response.data]))
+      .catch((error) => console.error(error));
+    setTask("");
+  };
+
+  const deleteTodo = (id) => {
+    axios.delete(`http://localhost:5000/api/todos/${id}`)
+      .then(() => setTodos(todos.filter(todo => todo.id !== id)))
+      .catch((error) => console.error(error));
+  };
+
+  return (
+    <div>
+      <h1>To-Do List</h1>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.task}
+            <button onClick={() => deleteTodo(todo.id)}>X</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
